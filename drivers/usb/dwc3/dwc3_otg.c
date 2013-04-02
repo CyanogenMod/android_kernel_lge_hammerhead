b548f8931d35e667363a006e2cf34e2b4a43024d
jihyun.seong <jihyun.seong@lge.com>
2013-04-02 16:35:51 +0900

    slimport: support 500mA charging with slimport.
    dwc3_otg codes will be used for slimport charge detection.
    when TA is inserted to slimport cable, (while cable connected to usb slot)
    500mA charging will activated.
    
    Change-Id: Ie7f53f967d3e912ae395d0a1adee33ad9aa56277

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -18,6 +18,7 @@
 #include <linux/usb/hcd.h>
 #include <linux/platform_device.h>
 #include <linux/regulator/consumer.h>
+#include <linux/slimport.h>
 
 #include "core.h"
 #include "dwc3_otg.h"
@@ -559,6 +560,9 @@ static int dwc3_otg_set_power(struct usb_phy *phy, unsigned mA)
 	if (dotg->charger->chg_type == DWC3_CDP_CHARGER)
 		mA = DWC3_IDEV_CHG_MAX;
 
+	if (slimport_is_connected())
+		mA = 500;
+
 	if (dotg->charger->max_power == mA)
 		return 0;
 
@@ -744,6 +748,10 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 	case OTG_STATE_B_IDLE:
 		if (!test_bit(ID, &dotg->inputs)) {
 			dev_dbg(phy->dev, "!id\n");
+			if (slimport_is_connected()) {
+				work = 1;
+				break;
+			}
 			phy->state = OTG_STATE_A_IDLE;
 			work = 1;
 			dotg->charger_retry_count = 0;
@@ -776,10 +784,14 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 					work = 1;
 					break;
 				case DWC3_SDP_CHARGER:
-					dwc3_otg_start_peripheral(&dotg->otg,
-									1);
-					phy->state = OTG_STATE_B_PERIPHERAL;
-					work = 1;
+					if (!slimport_is_connected()) {
+						dwc3_otg_start_peripheral(
+								&dotg->otg,
+								1);
+						phy->state =
+							OTG_STATE_B_PERIPHERAL;
+						work = 1;
+					}
 					break;
 				case DWC3_FLOATED_CHARGER:
 					if (dotg->charger_retry_count <
