40794567f7ca97a34d294e6eb8c6c4b4de202777
Aravind Venkateswaran <aravindh@codeaurora.org>
2013-07-06 22:42:58 -0700

    msm: clock-mdss: Do not toggle lock detect during handoff
    If continuous splash screen feature is enabled, then the DSI
    clocks are left on in the bootloader. When the kernel boots
    up, te DSI PLL lock status is checked in order to determine
    whether these clocks should be handed off or not. Ensure that
    the lock detect register is not toggled at this time to avoid
    any blinks in the display.
    
    Change-Id: Ica8c42e260d9b3ceffda885b1942fab945f60c86
    CRs-Fixed: 506037
    Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>

--- arch/arm/mach-msm/clock-mdss-8974.c
+++ arch/arm/mach-msm/clock-mdss-8974.c
@@ -1043,8 +1043,6 @@ static int dsi_pll_lock_status(void)
 	u32 status;
 	int pll_locked = 0;
 
-	dsi_pll_toggle_lock_detect();
-
 	/* poll for PLL ready status */
 	if (readl_poll_timeout_noirq((mdss_dsi_base +
 			DSI_0_PHY_PLL_UNIPHY_PLL_STATUS),
@@ -1098,7 +1096,7 @@ static int dsi_pll_enable_seq_m(void)
 	DSS_REG_W(mdss_dsi_base, DSI_0_PHY_PLL_UNIPHY_PLL_GLB_CFG, 0x0f);
 	udelay(1000);
 
-	pll_locked = dsi_pll_lock_status();
+	pll_locked = dsi_pll_toggle_lock_detect_and_check_status();
 	for (i = 0; (i < 4) && !pll_locked; i++) {
 		DSS_REG_W(mdss_dsi_base,
 			DSI_0_PHY_PLL_UNIPHY_PLL_GLB_CFG, 0x07);
@@ -1109,7 +1107,7 @@ static int dsi_pll_enable_seq_m(void)
 		DSS_REG_W(mdss_dsi_base,
 			DSI_0_PHY_PLL_UNIPHY_PLL_GLB_CFG, 0x0f);
 		udelay(1000);
-		pll_locked = dsi_pll_lock_status();
+		pll_locked = dsi_pll_toggle_lock_detect_and_check_status();
 	}
 
 	if (pll_locked)
