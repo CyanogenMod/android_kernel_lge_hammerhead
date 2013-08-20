6776cd2076f07e2c071f27bc683c13cf2b74c6dd
Xiaoming Zhou <zhoux@codeaurora.org>
2013-08-20 13:23:06 -0400

    msm: mdss: make the mdss debugfs implementation generic
    Remove the core specific details in the debugfs implementation via
    a defined debug interface.
    
    Change-Id: I2bf9ac47e0121f1f269d8d82274143d760305526
    Signed-off-by: Xiaoming Zhou <zhoux@codeaurora.org>

--- drivers/video/msm/mdss/mdss.h
+++ drivers/video/msm/mdss/mdss.h
@@ -71,6 +71,12 @@ struct mdss_intr {
 	spinlock_t lock;
 };
 
+struct mdss_debug_inf {
+	void *debug_data;
+	int (*debug_dump_stats)(void *data, char *buf, int len);
+	void (*debug_enable_clock)(int on);
+};
+
 struct mdss_data_type {
 	u32 mdp_rev;
 	struct clk *mdp_clk[MDSS_MAX_CLK];
@@ -152,7 +158,8 @@ struct mdss_data_type {
 	struct mdss_iommu_map_type *iommu_map;
 
 	struct early_suspend early_suspend;
-	void *debug_data;
+	struct mdss_debug_inf debug_inf;
+	int current_bus_idx;
 	bool mixer_switched;
 	bool ulps;
 
