eb387f7eedf864f01b7281d199ed73947d694762
Aditya Jonnalagadda <ajonnala@codeaurora.org>
2013-11-20 20:14:57 +0530

    msm: camera: Maintain buffer queue in isp buf manager
    we encountered a problem where we are trying to free one
    of buffers from previous ion session and this is resulting
    in a BUG_ON because of mismatch of ion_handles.
    
    Inorder to avoid this we are now maintaining a queue for
    all the buffers during prepare buf and we unprepare only
    if the buffer is present in this queue.
    
    Change-Id: Ifd526ec0e4dc9d83a7c1a095af97a833a8b82a57
    Signed-off-by: Aditya Jonnalagadda <ajonnala@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.h
+++ drivers/media/platform/msm/camera_v2/isp/msm_buf_mgr.h
@@ -56,6 +56,11 @@ struct msm_isp_buffer_mapped_info {
 	struct ion_handle *handle;
 };
 
+struct buffer_cmd {
+	struct list_head list;
+	struct msm_isp_buffer_mapped_info *mapped_info;
+};
+
 struct msm_isp_buffer {
 	/*Common Data structure*/
 	int num_planes;
@@ -153,6 +158,7 @@ struct msm_isp_buf_mgr {
 
 	int num_iommu_ctx;
 	struct device *iommu_ctx[2];
+	struct list_head buffer_q;
 };
 
 int msm_isp_create_isp_buf_mgr(struct msm_isp_buf_mgr *buf_mgr,
