f3c50fe14ba2256fb943d8a6d814d459f9e9c4e8
Ujwal Patel <ujwalp@codeaurora.org>
2013-11-21 08:21:44 -0800

    msm: mdss: release all overlay resources if calling process is unknown
    There are cases where fb release is called from a process which is
    unknown to internal process tracking list. This usually happens when
    a crash handlers closes open fds in a kernel worker thread rather than
    in process context. In such cases if fb open happens from a completely
    new process then it may lead to pipe leakage due to race condition
    between unknown process and this new, fb open, process. Prevent this by
    releasing all overlay resources when fb release is called from an
    unknown process context.
    
    Change-Id: I07157116413dfb26e0b87c61f9def916a653340a
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -83,7 +83,7 @@ struct msm_mdp_interface {
 	int (*on_fnc)(struct msm_fb_data_type *mfd);
 	int (*off_fnc)(struct msm_fb_data_type *mfd);
 	/* called to release resources associated to the process */
-	int (*release_fnc)(struct msm_fb_data_type *mfd);
+	int (*release_fnc)(struct msm_fb_data_type *mfd, bool release_all);
 	int (*kickoff_fnc)(struct msm_fb_data_type *mfd);
 	int (*ioctl_handler)(struct msm_fb_data_type *mfd, u32 cmd, void *arg);
 	void (*dma_fnc)(struct msm_fb_data_type *mfd);
