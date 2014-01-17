a01c4e8ddaac6e82b21ed9c2e4a1a54ad875a2f0
Mayank Chopra <makchopra@codeaurora.org>
2014-01-17 17:35:57 +0530

    msm: mdss: Reset pipe's mfd parameter during pipe free
    There is a corner case with dma pipe. If its set call
    fails as line mode (for mdp) and is configured
    subsequently as block mode (for rotation), mfd parameter
    of the dma pipe structure does not get reset during set
    failure cleanup. This causes unnecessary force dma
    cleanup call in rotatorp lay which gets hang and
    results in rotator fence timeout.
    
    Change-Id: Ia090e9eebf43ab6e28a2746f8fe57e2c3da34e59
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_pipe.c
+++ drivers/video/msm/mdss/mdss_mdp_pipe.c
@@ -642,6 +642,7 @@ static int mdss_mdp_pipe_free(struct mdss_mdp_pipe *pipe)
 	mdss_mdp_smp_free(pipe);
 	pipe->flags = 0;
 	pipe->bwc_mode = 0;
+	pipe->mfd = NULL;
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF, false);
 
 	return 0;
