a0369ba4a4dc29815d48e7f81f949a9f4386a675
Murali Nalajala <mnalajal@codeaurora.org>
2014-01-20 00:16:21 +0530

    mach-msm: Remove unused headers in power drivers
    All power drivers are SoC independent and required information
    is passed from the respective target specific DT files. Remove
    the unused header files in power drivers.
    
    Change-Id: I2efdd3b3d782f7575cae4d84b3ccce89028d073d
    Signed-off-by: Murali Nalajala <mnalajal@codeaurora.org>

--- arch/arm/mach-msm/test-lpm.c
+++ arch/arm/mach-msm/test-lpm.c
@@ -22,7 +22,6 @@
 #include <linux/ctype.h>
 #include <linux/moduleparam.h>
 #include <linux/platform_device.h>
-#include <mach/socinfo.h>
 #if defined(CONFIG_MSM_RPM)
 #include "rpm_resources.h"
 #endif
