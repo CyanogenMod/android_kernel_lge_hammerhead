f1d341b3e0fec756ad31689b4abfb5db41ce043f
Deva Ramasubramanian <dramasub@codeaurora.org>
2014-01-24 12:38:37 -0800

    media: Init the reserved fields of struct media_link_desc
    struct media_link_desc is copy_to_user'ed as the return value of
    MEDIA_IOC_ENUM_LINKS. When copying, the driver is omitting to initialise
    the reserved fields.  This commit fixes that by initialising the
    reserved fields to 0.
    
    Bug: 28750150
    CRs-Fixed: 570757
    Change-Id: I230e2666c0845cc36399518a0f2c94db664382d1
    Signed-off-by: Deva Ramasubramanian <dramasub@codeaurora.org>
    Signed-off-by: Siqi Lin <siqilin@google.com>

--- drivers/media/media-device.c
+++ drivers/media/media-device.c
@@ -139,7 +139,7 @@ static long media_device_enum_links(struct media_device *mdev,
 		unsigned int p;
 
 		for (p = 0; p < entity->num_pads; p++) {
-			struct media_pad_desc pad;
+			struct media_pad_desc pad = {0};
 
 			memset(&pad, 0, sizeof(pad));
 			media_device_kpad_to_upad(&entity->pads[p], &pad);
