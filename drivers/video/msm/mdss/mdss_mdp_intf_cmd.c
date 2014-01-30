53c2b788a66f2acb6f5e8113734c1f2593a54169
Steve Kondik <shade@chemlab.org>
2014-01-29 17:25:22 -0800

    Revert "msm: mdss: push wait for fences and ping pong done as late as possible"
    This reverts commit 3f904f2f89bb6c9914cf5621c2dc6719348ab2c2.

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -416,6 +416,7 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 	unsigned long flags;
 	int need_wait = 0;
 	int rc = 0;
+	int flush_wq = (int) arg;
 
 	ctx = (struct mdss_mdp_cmd_ctx *) ctl->priv_data;
 	if (!ctx) {
@@ -444,6 +445,9 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 			rc = 0;
 		}
 	}
+
+	if (flush_wq)
+		flush_work_sync(&ctx->pp_done_work);
 
 	return rc;
 }
@@ -538,9 +542,11 @@ int mdss_mdp_cmd_stop(struct mdss_mdp_ctl *ctl)
 	if (cancel_work_sync(&ctx->clk_work))
 		pr_debug("no pending clk work\n");
 
+	mdss_mdp_cmd_wait4pingpong(ctl, NULL);
+
 	mdss_mdp_cmd_clk_off(ctx);
 
-	flush_work(&ctx->pp_done_work);
+	flush_work_sync(&ctx->pp_done_work);
 
 	ctx->panel_on = 0;
 
