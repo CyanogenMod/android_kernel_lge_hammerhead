1674718b9df325150218f93f8b0c3a70f7273f87
myfluxi <linflux@arcor.de>
2014-02-06 21:21:44 +0100

    msm: mdss: Use interruptible threads
    Based on @cyanogen's findings, use interruptible threads to reduce
    cpu load.
    
    Change-Id: Ice983fd565d5defaa4a09e35d228bfd39f84c54e

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2287,7 +2287,7 @@ static int __mdss_fb_display_thread(void *data)
 				mfd->index);
 
 	while (1) {
-		wait_event(mfd->commit_wait_q,
+		wait_event_interruptible(mfd->commit_wait_q,
 				(atomic_read(&mfd->commits_pending) ||
 				 kthread_should_stop()));
 
