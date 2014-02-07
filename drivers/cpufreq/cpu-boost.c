bc5a369d3741461cc4e8d2260106d269217dc9ff
myfluxi <linflux@arcor.de>
2014-02-07 23:56:38 +0100

    cpufreq: cpu-boost: Remove unused waitqueue
    Change-Id: I2e72d8ee5f2777bffe8d7817122934d8857c78b6

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -27,7 +27,6 @@
 #include <linux/time.h>
 
 struct cpu_sync {
-	wait_queue_head_t sync_wq;
 	struct delayed_work boost_rem;
 	struct delayed_work input_boost_rem;
 	int cpu;
@@ -200,7 +199,6 @@ static int boost_migration_notify(struct notifier_block *nb,
 	s->pending = true;
 	s->src_cpu = (int) arg;
 	spin_unlock_irqrestore(&s->lock, flags);
-	wake_up(&s->sync_wq);
 
 	return NOTIFY_OK;
 }
@@ -341,7 +339,6 @@ static int cpu_boost_init(void)
 	for_each_possible_cpu(cpu) {
 		s = &per_cpu(sync_info, cpu);
 		s->cpu = cpu;
-		init_waitqueue_head(&s->sync_wq);
 		spin_lock_init(&s->lock);
 		INIT_DELAYED_WORK(&s->boost_rem, do_boost_rem);
 		INIT_DELAYED_WORK(&s->input_boost_rem, do_input_boost_rem);
