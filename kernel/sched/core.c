e7166d98253766affc69353704273d8cc1039a83
Rohit Gupta <rohgup@codeaurora.org>
2014-03-14 18:56:14 -0700

    cpufreq: cpu-boost: Introduce scheduler assisted load based syncs
    Previously, on getting a migration notification cpu-boost changed
    the scaling min of the destination frequency to match that of the
    source frequency or sync_threshold whichever was minimum.
    
    If the scheduler migration notification is extended with task load
    (cpu demand) information, the cpu boost driver can use this load to
    compute a suitable frequency for the migrating task. The required
    frequency for the task is calculated by taking the load percentage
    of the max frequency and no sync is performed if the load is less
    than a particular value (migration_load_threshold).This change is
    beneficial for both perf and power as demand of a task is taken into
    consideration while making cpufreq decisions and unnecessary syncs
    for lightweight tasks are avoided.
    
    The task load information provided by scheduler comes from a
    window-based load collection mechanism which also normalizes the
    load collected by the scheduler to the max possible frequency
    across all CPUs.
    
    Change-Id: Id2ba91cc4139c90602557f9b3801fb06b3c38992
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1784,9 +1784,19 @@ stat:
 out:
 	raw_spin_unlock_irqrestore(&p->pi_lock, flags);
 
-	if (notify)
+	if (src_cpu != cpu && task_notify_on_migrate(p)) {
+		struct migration_notify_data mnd;
+
+		mnd.src_cpu = src_cpu;
+		mnd.dest_cpu = cpu;
+		if (sysctl_sched_ravg_window)
+			mnd.load = div64_u64((u64)p->se.ravg.demand * 100,
+				(u64)(sysctl_sched_ravg_window));
+		else
+			mnd.load = 0;
 		atomic_notifier_call_chain(&migration_notifier_head,
-					   cpu, (void *)src_cpu);
+					   0, (void *)&mnd);
+	}
 	return success;
 }
 
@@ -5274,9 +5284,19 @@ done:
 fail:
 	double_rq_unlock(rq_src, rq_dest);
 	raw_spin_unlock(&p->pi_lock);
-	if (moved && task_notify_on_migrate(p))
+	if (moved && task_notify_on_migrate(p)) {
+		struct migration_notify_data mnd;
+
+		mnd.src_cpu = src_cpu;
+		mnd.dest_cpu = dest_cpu;
+		if (sysctl_sched_ravg_window)
+			mnd.load = div64_u64((u64)p->se.ravg.demand * 100,
+				(u64)(sysctl_sched_ravg_window));
+		else
+			mnd.load = 0;
 		atomic_notifier_call_chain(&migration_notifier_head,
-					   dest_cpu, (void *)src_cpu);
+					   0, (void *)&mnd);
+	}
 	return ret;
 }
 
