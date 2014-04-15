926206882b034f6d317506dc428e1d6a3353e542
myfluxi <linflux@arcor.de>
2014-04-15 21:04:22 +0200

    Revert "mm, show_mem: suppress page counts in non-blockable contexts"
    This reverts commit 87722ea22cd69f8a72b0bf5673332a3898349c53.
    
    Change-Id: I15e69166b4e3f849245e435ab486e4323c4887f7

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -894,8 +894,7 @@ extern void pagefault_out_of_memory(void);
  * Flags passed to show_mem() and show_free_areas() to suppress output in
  * various contexts.
  */
-#define SHOW_MEM_FILTER_NODES		(0x0001u)	/* disallowed nodes */
-#define SHOW_MEM_FILTER_PAGE_COUNT	(0x0002u)	/* page type count */
+#define SHOW_MEM_FILTER_NODES	(0x0001u)	/* filter disallowed nodes */
 
 extern void show_free_areas(unsigned int flags);
 extern bool skip_free_areas_node(unsigned int flags, int nid);
