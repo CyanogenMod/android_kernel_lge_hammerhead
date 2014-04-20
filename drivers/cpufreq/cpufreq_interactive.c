910b01e82c54b3a3d4dff3b2d7202def1561de37
eak1080 <ethan.kaley@gmail.com>
2014-04-19 20:35:01 -0400

    Trying something out...an attempt to smooth the jumping timer
    Change-Id: I84d103bfdb026bcf54d72339408c8f314722e7ac

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -427,7 +427,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	pcpu->prev_load = cpu_load;
 	boosted = boost_val || now < boostpulse_endtime;
 
-	if (cpu_load >= go_hispeed_load || boosted) {
+	if (cpu_load >= go_hispeed_load) {
 		if (pcpu->target_freq < hispeed_freq) {
 			new_freq = hispeed_freq;
 		} else {
