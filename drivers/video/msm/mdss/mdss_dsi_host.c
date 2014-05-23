e6c60f69ce758aadd367be6e9b675f0af7dd9df1
Kuogee Hsieh <khsieh@codeaurora.org>
2014-05-23 12:40:39 -0700

    mdss: display: extend mutex coverage inside cmdlist_put
    At Current implementation mutex is released after dcs
    commands had been added to queue and before cmdlist_commit
    called to tx commands. This is created a possibility to allow
    other thread do take over command queue and call cmdlist_commit
    to tx commands which not added by it. Extend mutex coverage to
    including both cmdlsit_put and cmdlist_commit to fix the problem.
    
    CRs-Fixed: 669136
    Change-Id: I317adae2b39505d96afe1edd27f1a1ab813b343d
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -1400,7 +1400,9 @@ int mdss_dsi_cmdlist_commit(struct mdss_dsi_ctrl_pdata *ctrl, int from_mdp)
 	if (mdss_get_sd_client_cnt())
 		return -EPERM;
 
-	mutex_lock(&ctrl->cmd_mutex);
+	if (from_mdp)	/* from mdp kickoff */
+		mutex_lock(&ctrl->cmd_mutex);
+
 	req = mdss_dsi_cmdlist_get(ctrl);
 
 	MDSS_XLOG(ctrl->ndx, from_mdp, ctrl->mdp_busy, current->pid,
@@ -1452,15 +1454,19 @@ int mdss_dsi_cmdlist_commit(struct mdss_dsi_ctrl_pdata *ctrl, int from_mdp)
 
 need_lock:
 
-	if (from_mdp) { /* from pipe_commit */
+	MDSS_XLOG(ctrl->ndx, from_mdp, ctrl->mdp_busy, current->pid,
+							XLOG_FUNC_EXIT);
+
+	if (from_mdp) { /* from mdp kickoff */
 		/* acquire lock only has new frame update */
 		if (ctrl->roi.w != 0 || ctrl->roi.h != 0)
 			mdss_dsi_cmd_mdp_start(ctrl);
+
+		mutex_unlock(&ctrl->cmd_mutex);
 	}
 
 	MDSS_XLOG(ctrl->ndx, from_mdp, ctrl->mdp_busy, current->pid,
 							XLOG_FUNC_EXIT);
-	mutex_unlock(&ctrl->cmd_mutex);
 	return ret;
 }
 
