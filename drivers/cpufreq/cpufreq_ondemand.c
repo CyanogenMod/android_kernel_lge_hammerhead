296c5263126bd8272523477cebdefc842095bc37
myfluxi <linflux@arcor.de>
2014-05-25 00:09:14 +0200

    cpufreq: ondemand: Calculate load_at_max_freq based on current policy->max
    Change-Id: I06b311b343f8137f276ec561c4b598722f663750

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -786,7 +786,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	}
 
 	/* calculate the scaled load across CPU */
-	load_at_max_freq = (cur_load * policy->cur)/policy->cpuinfo.max_freq;
+	load_at_max_freq = (cur_load * policy->cur)/policy->max;
 
 	cpufreq_notify_utilization(policy, load_at_max_freq);
 	/* Check for frequency increase */
