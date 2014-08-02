612d91163f47ea16438411e4e226935e3f1a5ff1
myfluxi <linflux@arcor.de>
2014-08-02 09:48:44 +0200

    msm: mdss: Make sure clocks are on during kickoff
    Avoid a race during kickoff when DCS commands are sent.
    Keep clocks on until our irq is enabled and the start
    bit is finally written.
    
    Change-Id: I2a7fe57cd9aff0b60bc7969ff514203ee5c72e1b

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -608,6 +608,7 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 
 	mdss_mdp_cmd_clk_on(ctx);
 
+	mutex_lock(&ctx->clk_mtx);
 	mdss_mdp_cmd_set_partial_roi(ctl);
 
 	/*
@@ -617,6 +618,7 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 	INIT_COMPLETION(ctx->pp_comp);
 	mdss_mdp_irq_enable(MDSS_MDP_IRQ_PING_PONG_COMP, ctx->pp_num);
 	mdss_mdp_ctl_write(ctl, MDSS_MDP_REG_CTL_START, 1);
+	mutex_unlock(&ctx->clk_mtx);
 	mdss_mdp_ctl_perf_set_transaction_status(ctl,
 		PERF_SW_COMMIT_STATE, PERF_STATUS_DONE);
 	mb();
