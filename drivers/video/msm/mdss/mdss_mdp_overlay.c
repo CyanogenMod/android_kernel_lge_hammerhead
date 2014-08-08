06e64911a7ba5102806a9381afe9be2e4fe134b4
myfluxi <linflux@arcor.de>
2014-08-08 19:33:52 +0200

    Revert "msm: mdss: Enable overfetch unconditionally"
    This reverts commit 0c2915e6a0c034e37dd6c983a9b80add5748b9a8.
    It fixed the time green line but broke neflix video playback.
    
    Change-Id: I39c5f992ed42256c7bfc0d6fba7f959bd26c2c4e

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -491,6 +491,8 @@ int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 	} else {
 		pipe->overfetch_disable = 0;
 	}
+	pipe->overfetch_disable = fmt->is_yuv &&
+			!(pipe->flags & MDP_SOURCE_ROTATED_90);
 	pipe->bg_color = req->bg_color;
 
 	req->id = pipe->ndx;
