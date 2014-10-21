41ec0843ecfd36df80d51c82407f3834e493cd70
Kuogee Hsieh <khsieh@codeaurora.org>
2014-10-21 10:14:13 -0700

    msm: mdss: reduce waiting time before turn off mdp clock
    Vsync happens at every 16.6 ms. Currently it wait for
    6 vsycn (100 ms) time before turned off mdp clock if there
    has no new frame update. Reduce 6 vsycn time to 4 have
    mdp clock turned off faster. During suspending procedure,
    change it to 1 so that mdp clock is turned off 1 vsycn
    after kickoff completed if there has kickoff pending.
    Otherwise mdp clock is turned off at next vsync.
    
    Change-Id: Ibb000b04cb4204ff63647dda3146c55084330687
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -35,6 +35,7 @@ struct mdss_mdp_cmd_ctx {
 	struct list_head vsync_handlers;
 	int panel_power_state;
 	atomic_t koff_cnt;
+	atomic_t intf_stopped;
 	int clk_enabled;
 	int vsync_enabled;
 	int rdptr_enabled;
@@ -647,6 +648,11 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 		return -ENODEV;
 	}
 
+	if (atomic_read(&ctx->intf_stopped)) {
+		pr_err("ctx=%d stopped already\n", ctx->pp_num);
+		return -EPERM;
+	}
+
 	mdss_mdp_ctl_perf_set_transaction_status(ctl,
 		PERF_HW_MDP_STATE, PERF_STATUS_BUSY);
 
@@ -731,10 +737,20 @@ int mdss_mdp_cmd_stop(struct mdss_mdp_ctl *ctl, int panel_power_state)
 				ctx->rdptr_enabled, XLOG_FUNC_ENTRY);
 
 	spin_lock_irqsave(&ctx->clk_lock, flags);
+	/* intf stopped,  no more kickoff */
+	atomic_set(&ctx->intf_stopped, 1);
 	if (ctx->rdptr_enabled) {
 		INIT_COMPLETION(ctx->stop_comp);
 		need_wait = 1;
+		/*
+		 * clk off at next vsync after pp_done  OR
+		 * next vsync if there has no kickoff pending
+		 */
+		ctx->rdptr_enabled = 1;
 	}
+	MDSS_XLOG(ctl->num, atomic_read(&ctx->koff_cnt),
+				atomic_read(&ctx->intf_stopped),
+				ctx->rdptr_enabled, need_wait);
 	spin_unlock_irqrestore(&ctx->clk_lock, flags);
 
 	if (need_wait) {
@@ -842,6 +858,8 @@ int mdss_mdp_cmd_start(struct mdss_mdp_ctl *ctl)
 
 	ctx->recovery.fxn = mdss_mdp_cmd_underflow_recovery;
 	ctx->recovery.data = ctx;
+
+	atomic_set(&ctx->intf_stopped, 0);
 
 	pr_debug("%s: ctx=%p num=%d mixer=%d\n", __func__,
 				ctx, ctx->pp_num, mixer->num);
