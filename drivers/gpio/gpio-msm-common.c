e8508768b25028fea188987f548324a6bf2b261b
myfluxi <linflux@arcor.de>
2015-02-13 17:24:55 +0100

    gpio: msm-common: Remove logging of wakeup reasons
    This conditionally reverts: Id71279f009220e017a3f4a687a75103cf7bdc3d5
    
    Change-Id: I9bc49f890029ef0b08399e66aa48e1e4a016d016

--- drivers/gpio/gpio-msm-common.c
+++ drivers/gpio/gpio-msm-common.c
@@ -25,7 +25,6 @@
 #include <linux/err.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
-#include <linux/wakeup_reason.h>
 
 #include <asm/mach/irq.h>
 
@@ -374,10 +373,8 @@ void msm_gpio_show_resume_irq(void)
 	spin_lock_irqsave(&tlmm_lock, irq_flags);
 	for_each_set_bit(i, msm_gpio.wake_irqs, ngpio) {
 		intstat = __msm_gpio_get_intr_status(i);
-		if (intstat) {
+		if (intstat)
 			irq = msm_gpio_to_irq(&msm_gpio.gpio_chip, i);
-			log_wakeup_reason(irq);
-		}
 	}
 	spin_unlock_irqrestore(&tlmm_lock, irq_flags);
 }
