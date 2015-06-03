5cf6c1a5736927fb713c739354fd70e90b69890e
myfluxi <linflux@arcor.de>
2015-06-03 23:01:18 +0200

    msm_serial_hs: Support for out of band sleep
    Change-Id: Ib223225233d3994753873584a73ae680823e033f

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -220,6 +220,7 @@ struct msm_hs_port {
 	bool rx_discard_flush_issued;
 	int rx_count_callback;
 	bool rx_bam_inprogress;
+	bool obs;
 };
 
 #define MSM_UARTDM_BURST_SIZE 16   /* DM burst size (in bytes) */
@@ -1936,12 +1937,19 @@ static int msm_hs_check_clock_off(struct uart_port *uport)
 	msm_hs_clock_unvote(msm_uport);
 
 	spin_lock_irqsave(&uport->lock, flags);
-	if (use_low_power_wakeup(msm_uport)) {
+	/* For out of band sleep wakeup interrupt is not required */
+	if (!msm_uport->obs && use_low_power_wakeup(msm_uport)) {
 		msm_uport->wakeup.ignore = 1;
 		enable_irq(msm_uport->wakeup.irq);
 	}
-	wake_unlock(&msm_uport->dma_wake_lock);
 
+	/*
+	 * keeping uport-irq enabled all the time
+	 * gates XO shutdown in idle power collapse.
+	 */
+	disable_irq(uport->irq);
+
+	wake_unlock(&msm_uport->dma_wake_lock);
 	spin_unlock_irqrestore(&uport->lock, flags);
 
 	/* Reset PNOC Bus Scaling */
@@ -2129,8 +2137,11 @@ void msm_hs_request_clock_on(struct uart_port *uport)
 	switch (msm_uport->clk_state) {
 	case MSM_HS_CLK_OFF:
 		wake_lock(&msm_uport->dma_wake_lock);
-		if (use_low_power_wakeup(msm_uport))
+		if (!msm_uport->obs && use_low_power_wakeup(msm_uport))
 			disable_irq_nosync(msm_uport->wakeup.irq);
+
+		/* uport-irq was disabled when clocked off */
+		enable_irq(uport->irq);
 		spin_unlock_irqrestore(&uport->lock, flags);
 
 		ret = msm_hs_clock_vote(msm_uport);
@@ -2687,6 +2698,11 @@ struct msm_serial_hs_platform_data
 	if (pdata->uart_rfr_gpio < 0)
 		pr_debug("uart_rfr_gpio is not available\n");
 
+	pdata->obs = of_property_read_bool(node,
+					"qcom,msm-obs");
+	if (pdata->obs)
+		pr_debug("%s: Out of Band Sleep is enabled\n", __func__);
+
 	pdata->inject_rx_on_wakeup = of_property_read_bool(node,
 				"qcom,inject-rx-on-wakeup");
 
@@ -3079,6 +3095,7 @@ static int __devinit msm_hs_probe(struct platform_device *pdev)
 		msm_uport->wakeup.ignore = 1;
 		msm_uport->wakeup.inject_rx = pdata->inject_rx_on_wakeup;
 		msm_uport->wakeup.rx_to_inject = pdata->rx_to_inject;
+		msm_uport->obs = pdata->obs;
 
 		if (unlikely(msm_uport->wakeup.irq < 0)) {
 			ret = -ENXIO;
