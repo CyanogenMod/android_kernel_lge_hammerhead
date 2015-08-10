e5fa52020e71087cff75371ec7cfe7727c24bf7a
Raghavendra Ambadas <rambad@codeaurora.org>
2015-08-10 15:02:21 +0530

    msm: mdss: Acquire lock for source format b/w line and block mode
    Currently there is a page fault and flicker while programming
    DMA pipe registers in Line mode. To prevent this acquire wb_lock
    between writeback kickoff (N-Cycle) and rotator setup(N+1 Cycle).
    wb_lock avoid rotator to update the pipe structure before writeback
    is finished programming to the hardware.
    
    Change-Id: I589032831ec1813f12ce3f91e3aa8a6b187ebfdd
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1198,6 +1198,7 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd,
 		mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_BEGIN);
 		mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_READY);
 		mutex_lock(ctl->shared_lock);
+		mutex_lock(ctl->wb_lock);
 	}
 
 	mutex_lock(&mdp5_data->ov_lock);
@@ -1318,8 +1319,10 @@ commit_fail:
 		mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_START);
 
 	mutex_unlock(&mdp5_data->ov_lock);
-	if (ctl->shared_lock)
+	if (ctl->shared_lock) {
+		mutex_unlock(ctl->wb_lock);
 		mutex_unlock(ctl->shared_lock);
+	}
 	mdss_iommu_ctrl(0);
 	ATRACE_END(__func__);
 
