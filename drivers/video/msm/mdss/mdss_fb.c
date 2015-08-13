5501598f3e9d14e22c6ef07f136adad744433fdc
myfluxi <linflux@arcor.de>
2015-08-13 21:19:19 +0200

    Revert "msm: mdss: always save backlight brightness using unset_bl_level"
    This does not go along too well with our (U)LP stuff.
    
    This reverts commit e7479a1d71aac0ed78c4e4d37d8cc67cd8d4de62.
    
    Change-Id: Icb0fa8cbb23740a3d0e81e736487908d96f22e41

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1138,7 +1138,6 @@ static int mdss_fb_blank_blank(struct msm_fb_data_type *mfd,
 	int req_power_state)
 {
 	int ret = 0;
-	int bl_level_old;
 	int cur_power_state;
 
 	if (!mfd)
@@ -1168,16 +1167,11 @@ static int mdss_fb_blank_blank(struct msm_fb_data_type *mfd,
 
 	mfd->op_enable = false;
 	mutex_lock(&mfd->bl_lock);
-	if (mfd->bl_updated)
-		bl_level_old = mfd->bl_level;
-	else
-		bl_level_old = mfd->unset_bl_level;
 	if (mdss_panel_is_power_off(req_power_state)) {
 		/* Stop Display thread */
 		if (mfd->disp_thread)
 			mdss_fb_stop_disp_thread(mfd);
 		mdss_fb_set_backlight(mfd, 0);
-		mfd->unset_bl_level = bl_level_old;
 		mfd->bl_updated = 0;
 	}
 	mfd->panel_power_state = req_power_state;
@@ -1236,6 +1230,16 @@ static int mdss_fb_blank_unblank(struct msm_fb_data_type *mfd)
 		if (mfd->idle_time)
 			schedule_delayed_work(&mfd->idle_notify_work,
 				msecs_to_jiffies(mfd->idle_time));
+	}
+
+	/* Reset the backlight only if the panel was off */
+	if (mdss_panel_is_power_off(cur_power_state)) {
+		mutex_lock(&mfd->bl_lock);
+		if (!mfd->bl_updated) {
+			mfd->bl_updated = 1;
+			mdss_fb_set_backlight(mfd, mfd->unset_bl_level);
+		}
+		mutex_unlock(&mfd->bl_lock);
 	}
 
 error:
