cf06b65867ff03f5d11316ceb9024e2ca1496a3d
Naseer Ahmed <naseer@codeaurora.org>
2016-04-18 13:35:33 -0400

    msm: mdss: fix deadlock between display and dsi status check thread
    The vsync handler in display thread holds ov_lock followed by
    offlock. And the DSI status check thread for cmd mode panel holds
    offlock followed by ovlock, causing a clear deadlock scenario.
    Fix it by acquiring ov_lock before offlock in DSI status check
    thread.
    
    bug 28198793
    
    Change-Id: I54ef54b5ca7e4c87e909c510b28ff7770bb20a3b
    Signed-off-by: Veera Sundaram Sankaran <veeras@codeaurora.org>

--- drivers/video/msm/mdss/dsi_status_6g.c
+++ drivers/video/msm/mdss/dsi_status_6g.c
@@ -83,15 +83,15 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 	 * overlay operations. Need refine this lock for command mode
 	 */
 
-	mutex_lock(&ctl->offlock);
 	if (mipi->mode == DSI_CMD_MODE)
 		mutex_lock(&mdp5_data->ov_lock);
+	mutex_lock(&ctl->offlock);
 
 	if (mdss_panel_is_power_off(pstatus_data->mfd->panel_power_state) ||
 			pstatus_data->mfd->shutdown_pending) {
+		mutex_unlock(&ctl->offlock);
 		if (mipi->mode == DSI_CMD_MODE)
 			mutex_unlock(&mdp5_data->ov_lock);
-		mutex_unlock(&ctl->offlock);
 		pr_err("%s: DSI turning off, avoiding panel status check\n",
 							__func__);
 		return;
@@ -116,9 +116,9 @@ void mdss_check_dsi_ctrl_status(struct work_struct *work, uint32_t interval)
 	ret = ctrl_pdata->check_status(ctrl_pdata);
 	mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_OFF);
 
+	mutex_unlock(&ctl->offlock);
 	if (mipi->mode == DSI_CMD_MODE)
 		mutex_unlock(&mdp5_data->ov_lock);
-	mutex_unlock(&ctl->offlock);
 
 	if ((pstatus_data->mfd->panel_power_state == MDSS_PANEL_POWER_ON)) {
 		if (ret > 0) {
