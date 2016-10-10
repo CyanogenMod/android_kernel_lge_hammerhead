8be2d18744ab1dca31b54741cbf0005b1e8eb846
Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
2016-10-10 12:51:24 +0800

    nput: synaptics_dsx: allocate heap memory for temp buf
    There is a possible stack overflow vulnerability in the rmidev_write
    function because the stack array size is from user space.
    changes to allocate heap memory for the temporary buffer instead of
    stack memory to prevent the stack overflow vulnerability.
    As discussed under  CVE-2016-3865 and ANDROID-28799389.
    
    Change-Id: I20f639e09aaf3c533c98a12a2413570feae3d6d0
    Signed-off-by: Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
    Signed-off-by: Shantanu Jain <shjain@codeaurora.org>

--- drivers/input/touchscreen/synaptics_rmi_dev.c
+++ drivers/input/touchscreen/synaptics_rmi_dev.c
@@ -291,7 +291,7 @@ static ssize_t rmidev_read(struct file *filp, char __user *buf,
 		size_t count, loff_t *f_pos)
 {
 	ssize_t retval;
-	unsigned char tmpbuf[count + 1];
+	unsigned char *tmpbuf;
 	struct rmidev_data *dev_data = filp->private_data;
 
 	if (IS_ERR(dev_data)) {
@@ -304,7 +304,9 @@ static ssize_t rmidev_read(struct file *filp, char __user *buf,
 
 	if (count > (REG_ADDR_LIMIT - *f_pos))
 		count = REG_ADDR_LIMIT - *f_pos;
-
+	tmpbuf = kzalloc(count + 1, GFP_KERNEL);
+	if (!tmpbuf)
+		return -ENOMEM;
 	mutex_lock(&(dev_data->file_mutex));
 
 	retval = rmidev->fn_ptr->read(rmidev->rmi4_data,
@@ -321,7 +323,7 @@ static ssize_t rmidev_read(struct file *filp, char __user *buf,
 
 clean_up:
 	mutex_unlock(&(dev_data->file_mutex));
-
+	kfree(tmpbuf);
 	return retval;
 }
 
@@ -337,7 +339,7 @@ static ssize_t rmidev_write(struct file *filp, const char __user *buf,
 		size_t count, loff_t *f_pos)
 {
 	ssize_t retval;
-	unsigned char tmpbuf[count + 1];
+	unsigned char *tmpbuf;
 	struct rmidev_data *dev_data = filp->private_data;
 
 	if (IS_ERR(dev_data)) {
@@ -351,9 +353,15 @@ static ssize_t rmidev_write(struct file *filp, const char __user *buf,
 	if (count > (REG_ADDR_LIMIT - *f_pos))
 		count = REG_ADDR_LIMIT - *f_pos;
 
-	if (copy_from_user(tmpbuf, buf, count))
-		return -EFAULT;
+        tmpbuf = kzalloc(count + 1, GFP_KERNEL);
+        if (!tmpbuf)
+                return -ENOMEM;
 
+	if (copy_from_user(tmpbuf, buf, count))
+	{
+		kfree(tmpbuf);
+		return -EFAULT;
+	}
 	mutex_lock(&(dev_data->file_mutex));
 
 	retval = rmidev->fn_ptr->write(rmidev->rmi4_data,
@@ -364,7 +372,7 @@ static ssize_t rmidev_write(struct file *filp, const char __user *buf,
 		*f_pos += retval;
 
 	mutex_unlock(&(dev_data->file_mutex));
-
+	free(tmpbuf);
 	return retval;
 }
 
