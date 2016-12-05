ff35c5db4af7bea16308906335e279adaaa2468d
myfluxi <linflux@arcor.de>
2016-12-05 21:57:43 +0100

    printk: Ignore LOG_USER messages in kmsg
    Print only fatal log level messages.
    Set in:  /sys/module/printk/parameters/user_log_level
    
    Change-Id: Ie9c2206f769f912cae47c689b98317042ed57266

--- kernel/printk.c
+++ kernel/printk.c
@@ -259,6 +259,9 @@ static u32 log_buf_len = __LOG_BUF_LEN;
 /* cpu currently holding logbuf_lock */
 static volatile unsigned int logbuf_cpu = UINT_MAX;
 
+static unsigned int user_log_level = 2;
+module_param(user_log_level, uint, S_IRUGO | S_IWUSR);
+
 /* human readable text of the record */
 static char *log_text(const struct log *msg)
 {
@@ -411,6 +414,10 @@ static ssize_t devkmsg_writev(struct kiocb *iocb, const struct iovec *iv,
 		i = simple_strtoul(line+1, &endp, 10);
 		if (endp && endp[0] == '>') {
 			level = i & 7;
+			if (level > user_log_level) {
+				ret = 0;
+				goto out;
+			}
 			if (i >> 3)
 				facility = i >> 3;
 			endp++;
